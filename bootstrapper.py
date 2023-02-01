a132171d7c49875f01aaa88d09b9e32e926bca23
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-02-01 11:04:05 +0900

    bootstrap

--- bootstrapper.py
+++ bootstrapper.py
@@ -1,13 +1,22 @@
 import util
 import mediapipe as mp
+import argparse
+import os
+
 mp_pose = mp.solutions.pose
 
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', type=argparse.FileType())
+parser.add_argument('--out', '-o', help='부트스트래핑한 CSV파일을 내보낼 디렉토리를 입력받습니다')
+parser.add_argument('--complexity', '-c', help='모델 복잡도를 설정합니다', default=1)
+# parser.add_argument('--progress', '-p', help='프로그래스바를 출력합니다', action='store_true', default=False)
+
+args = parser.parse_args()
+args.filename = args.filename.name
+
 bootstrapper = util.Bootstrapper(mp_pose.Pose(            
-    model_complexity=2,
+    model_complexity=args.complexity,
     static_image_mode=True))
 up = bootstrapper('./data/test/up.mp4')
-down = bootstrapper('./data/test/down.mp4')
-
 csv_saver = util.CSVSaver()
-csv_saver('./data/test/up', up)
-csv_saver('./data/test/down', down)
+csv_saver(os.path.splitext(args.filename)[0], up)
